bc219c8853b8af3cf57335521a67c31448d49b4b
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-15 03:54:12 +0300

    fix: add await keyword to wait the random delay and return it

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,8 +5,11 @@ that waits for a random delay between 0 and max_delay
 (included and float value) seconds and eventually returns it.
 '''
 import random
+import asyncio
 
 
 async def wait_random(max_delay=10):
     '''asynchronous coroutine that takes in an integer argument'''
-    return random.uniform(0, max_delay)
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
