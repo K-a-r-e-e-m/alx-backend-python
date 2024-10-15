87d4ca3dcab240b20439799f487764b5560e4da3
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-15 04:44:16 +0300

    fix: add float to data annotation of List type

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,12 @@
 '''async routine called wait_n that takes in 2 int
 arguments (in this order): n and max_delay'''
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''takes in 2 int arguments n and max_delay
        and return the list of all the delays'''
     tasks = [asyncio.create_task(wait_random(max_delay=max_delay))
