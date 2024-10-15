fd14596b3623f1eff9f2f9bb4b7a0a65b8890cbb
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-15 04:01:56 +0300

    fix: add type annotation for max delay argument and return type

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     '''asynchronous coroutine that takes in an integer argument'''
     random_delay = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
